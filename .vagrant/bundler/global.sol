60111b702572999b0022e3c584cbcce504165dd3
gustavohenriqueT <gustavo.h.silva@sou.unifeob.edu.br>
2025-03-12 20:08:07 -0300

    Criando VM

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"82c44cc42f5085bef438bb21409f72be76da1145be32467f31b54d035b298f51","vagrant_version":"2.4.3"}
