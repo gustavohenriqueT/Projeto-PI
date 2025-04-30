d23145b4eaea63942fd9eb21ff904985bf481981
gustavohenriqueT <gustavo.h.silva@sou.unifeob.edu.br>
2025-04-29 21:58:35 -0300

    alteracoes

--- banco/init.sql
+++ banco/init.sql
@@ -1,12 +1,13 @@
 -- Cria tabela para armazenar os dados processados
 CREATE TABLE IF NOT EXISTS dados_transporte (
-    data_hora TIMESTAMP,
-    id_veiculo VARCHAR(50),
-    linha VARCHAR(50),
-    latitude FLOAT,
-    longitude FLOAT,
-    passageiros INTEGER,
-    tempo_viagem INTEGER,
-    horario_pico BOOLEAN,
-    situacao VARCHAR(10)
-);
+    Data_Hora DATETIME),
+    ID_Veiculo VARCHAR(50),
+    Linha VARCHAR(50),
+    Latitude DECIMAL(10, 6),
+    Longitude DECIMAL(10, 6),
+    Numero_Passageiros INT,
+    Tempo_Viagem_Minutos INT,
+    Dado_Correto BOOLEAN,
+    Hora INT,
+    Horario_Pico INT,
+    Situacao VARCHAR(10)
